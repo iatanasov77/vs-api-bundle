f1b35166274104c5e0f8304a4d092398d96aed11
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-03-06 08:47:37 +0000

    Add Configuration for API Title.

---
+++ lib/DependencyInjection/PrependApiPlatformTrait.php
@@ -0,0 +1,34 @@
+<?php namespace Vankosoft\PaymentBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+trait PrependApiPlatformTrait
+{
+    private function prependApiPlatform( ContainerBuilder $container ): void
+    {
+        if ( ! $container->hasExtension( 'api_platform' ) ) {
+            return;
+        }
+        
+        $vsApiConfig    = $container->getExtensionConfig( 'vs_api' );
+        //echo "<pre>"; var_dump($vsPaymentConfig); die;
+        $vsPaymentResources = $vsPaymentConfig[0]['resources'];
+        
+        $apiPlatformConfig        = $container->getExtensionConfig( 'api_platform' );
+        $container->prependExtensionConfig( 'api_platform', [
+            'title'  => $vsApiConfig['title']
+        ]);
+
+        //$this->debug( $container );
+    }
+    
+    private function debug( ContainerBuilder $container )
+    {
+        echo '<pre>';
+        //var_dump( $container->getParameter( 'vs_payment.model.gateway_config.class' ) );
+        echo '<br><br><br><br>';
+        var_dump( $container->getExtensionConfig( 'vs_api' ) );
+        echo '<br><br><br><br>';
+        var_dump( $container->getExtensionConfig( 'api_platform' ) ); die;
+    }
+}
