13023b6bd6b41546e1b3a820ff54179a754e4ec0
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-08-01 08:09:43 +0000

    Create a Refresh Token in API Manager.

--- lib/Security/ApiManager.php
+++ lib/Security/ApiManager.php
@@ -3,6 +3,8 @@
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Model\VerifyEmailSignatureComponents;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
@@ -16,17 +18,27 @@ class ApiManager
     /** @var JWTTokenManagerInterface */
     private $jwtManager;
     
+    /** @var RefreshTokenGeneratorInterface */
+    private $refreshTokenGenerator;
+    
+    /** @var RefreshTokenManagerInterface */
+    private $refreshTokenManager;
+    
     /** @var VerifyEmailHelperInterface */
     private $verifyEmailHelper;
     
     public function __construct(
         TokenStorageInterface $tokenStorage,
         JWTTokenManagerInterface $jwtManager,
+        RefreshTokenGeneratorInterface $refreshTokenGenerator,
+        RefreshTokenManagerInterface $refreshTokenManager,
         VerifyEmailHelperInterface $helper
     ) {
-        $this->tokenStorage         = $tokenStorage;
-        $this->jwtManager           = $jwtManager;
-        $this->verifyEmailHelper    = $helper;
+        $this->tokenStorage             = $tokenStorage;
+        $this->jwtManager               = $jwtManager;
+        $this->refreshTokenGenerator    = $refreshTokenGenerator;
+        $this->refreshTokenManager      = $refreshTokenManager;
+        $this->verifyEmailHelper        = $helper;
     }
     
     public function getToken()
@@ -68,9 +80,14 @@ class ApiManager
     {
         $tokenString    = $this->jwtManager->create( $oUser );
         
+        /** EXAMPLE: https://github.com/markitosgv/JWTRefreshTokenBundle/issues/322 */
+        $refreshToken = $this->refreshTokenGenerator->createForUserWithTtl( $user, SELF::TTL );
+        $this->refreshTokenManager->save( $refreshToken );
+        
         return [
             'tokenString'   => $tokenString,
-            'token'         => $this->jwtManager->parse( $tokenString )
+            'token'         => $this->jwtManager->parse( $tokenString ),
+            'refreshToken'  => $refreshToken,
         ];
     }
 }
